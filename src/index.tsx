63c4b986e43be256e77a761b65f63db0727b40f4
Colson <colsoncalifornication@gmail.com>
2023-09-20 14:51:08 +0545

    implement refs

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,17 @@
 import * as esbuild from 'esbuild-wasm';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import ReactDOM from 'react-dom/client';
 
 const App = () => {
+  const ref = useRef<any>();
   const [input, setInput] = useState('');
   const [code, setCode] = useState('');
 
   const startService = async () => {
-    const service = await esbuild.startService({
+    ref.current = await esbuild.startService({
       worker: true,
       wasmURL: '/esbuild.wasm',
     });
-    console.log(service);
   };
 
   useEffect(() => {
@@ -19,7 +19,11 @@ const App = () => {
   }, []);
 
   const onClick = () => {
-    console.log(input);
+    if (!ref.current) {
+      return;
+    }
+
+    console.log(ref.current);
   };
 
   return (
