bd868a8a7b835746285b3a32922833441e715766
Colson <colsoncalifornication@gmail.com>
2023-09-24 15:01:11 +0545

    set node environment process to production

--- src/index.tsx
+++ src/index.tsx
@@ -36,9 +36,13 @@ const App = () => {
       bundle: true,
       write: false,
       plugins: [unpkgPathPlugin()],
+      define: {
+        'process.env.NODE_ENV': '"production"',
+        global: 'window',
+      },
     });
 
-    // console.log(result); 
+    // console.log(result);
 
     setCode(result.outputFiles[0].text);
   };
--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -40,8 +40,8 @@ export const unpkgPathPlugin = () => {
           return {
             loader: 'jsx',
             contents: `
-              const message = require('nested-test-pkg');
-              console.log(message);
+              import React, { useState } from 'react';
+              console.log(React, useState);
             `,
           };
         }
