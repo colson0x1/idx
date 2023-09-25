561f1846b315b8f3c8ef3c48099580de5d3f3729
Colson <colsoncalifornication@gmail.com>
2023-09-25 07:42:32 +0545

    resolve and refactor onResolve logic with filters

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -19,25 +19,27 @@ export const unpkgPathPlugin = (inputCode: string) => {
   return {
     name: 'unpkg-path-plugin',
     setup(build: esbuild.PluginBuild) {
+      // @ Handle root entry file of 'index.js'
       // @ Figure out where index.js file is stored and all the modules within it
       // Overwrite ESBuild's natural process of trying to figure out
       // where a file is stored, what the path to it is
+      build.onResolve({ filter: /(^index\.js$)/ }, () => {
+        return { path: 'index.js', namespace: 'a' };
+      });
+
+      // @ Handle relative paths in a module
+      // Check to see if the file path is relative [./ or ../]
+      build.onResolve({ filter: /^\.+\// }, (args: any) => {
+        return {
+          namespace: 'a',
+          path: new URL(args.path, 'https://unpkg.com' + args.resolveDir + '/')
+            .href,
+        };
+      });
+
+      // @ Handle main file of a module
+      // Resolve root module name [Ex: src/]
       build.onResolve({ filter: /.*/ }, async (args: any) => {
-        console.log('onResolve', args);
-        if (args.path === 'index.js') {
-          return { path: args.path, namespace: 'a' };
-        }
-
-        if (args.path.includes('./') || args.path.includes('../')) {
-          return {
-            namespace: 'a',
-            path: new URL(
-              args.path,
-              'https://unpkg.com' + args.resolveDir + '/'
-            ).href,
-          };
-        }
-
         return {
           namespace: 'a',
           path: `https://unpkg.com/${args.path}`,
