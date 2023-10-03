8f9ec9ee095c7faacbcb6cb65219e91817bf4216
Colson <colsoncalifornication@gmail.com>
2023-10-03 19:01:16 +0545

    add type defination for onEditorDidMount

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -1,4 +1,4 @@
-import MonacoEditor from '@monaco-editor/react';
+import MonacoEditor, { EditorDidMount } from '@monaco-editor/react';
 
 interface CodeEditorProps {
   initialValue: string;
@@ -6,10 +6,12 @@ interface CodeEditorProps {
 }
 
 const CodeEditor: React.FC<CodeEditorProps> = ({ onChange, initialValue }) => {
-  const onEditorDidMount = (getValue: () => string, monacoEditor: any) => {
+  const onEditorDidMount: EditorDidMount = (getValue, monacoEditor) => {
     monacoEditor.onDidChangeModelContent(() => {
       onChange(getValue());
     });
+
+    monacoEditor.getModel()?.updateOptions({ tabSize: 2 });
   };
 
   return (
