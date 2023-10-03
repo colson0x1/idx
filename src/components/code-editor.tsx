ce364960f6c5418479cd2bc33c0a790cc09cd0ac
Colson <colsoncalifornication@gmail.com>
2023-10-03 17:32:09 +0545

    add additional props to the options configuration object

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -8,6 +8,13 @@ const CodeEditor = () => {
       height="500px"
       options={{
         wordWrap: 'on',
+        minimap: { enabled: false },
+        showUnused: false,
+        folding: false,
+        lineNumbersMinChars: 3,
+        fontSize: 18,
+        scrollBeyondLastLine: false,
+        automaticLayout: true,
       }}
     />
   );
