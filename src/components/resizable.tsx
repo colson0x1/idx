0aa47341bd1c2867cc68a825b48650e2eb67bccd
Colson <colsoncalifornication@gmail.com>
2023-10-08 15:16:20 +0545

    configure resize handle

--- src/components/resizable.tsx
+++ src/components/resizable.tsx
@@ -6,7 +6,13 @@ interface ResizableProps {
 }
 
 const Resizable: React.FC<ResizableProps> = ({ direction, children }) => {
-  return <div>{children}</div>;
+  return (
+    <ResizableBox height={300} width={300}>
+      {' '}
+      resizeHandles={['s']}
+      {children}
+    </ResizableBox>
+  );
 };
 
 export default Resizable;
