d5552aac8d519205ad7f41d46dafe73c69496841
Colson <colsoncalifornication@gmail.com>
2023-10-09 21:44:22 +0545

    log onresize events

--- src/components/resizable.tsx
+++ src/components/resizable.tsx
@@ -1,4 +1,5 @@
 import './resizable.css';
+import { useEffect } from 'react';
 import { ResizableBox, ResizableBoxProps } from 'react-resizable';
 
 interface ResizableProps {
@@ -8,6 +9,17 @@ interface ResizableProps {
 
 const Resizable: React.FC<ResizableProps> = ({ direction, children }) => {
   let resizableProps: ResizableBoxProps;
+
+  useEffect(() => {
+    const listener = () => {
+      console.log(window.innerWidth, window.innerHeight);
+    };
+    window.addEventListener('resize', listener);
+
+    return () => {
+      window.removeEventListener('resize', listener);
+    };
+  }, []);
 
   if (direction === 'horizontal') {
     resizableProps = {
