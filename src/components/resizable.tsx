894d070972d5bcabb5778e035fa26145fbdf8e42
Colson <colsoncalifornication@gmail.com>
2023-10-09 22:30:34 +0545

    improve resize performance by debouncing

--- src/components/resizable.tsx
+++ src/components/resizable.tsx
@@ -13,9 +13,16 @@ const Resizable: React.FC<ResizableProps> = ({ direction, children }) => {
   const [innerWidth, setInnerWidth] = useState(window.innerWidth);
 
   useEffect(() => {
+    let timer: any;
+
     const listener = () => {
-      setInnerHeight(window.innerHeight);
-      setInnerWidth(window.innerWidth);
+      if (timer) {
+        clearTimeout(timer);
+      }
+      timer = setTimeout(() => {
+        setInnerHeight(window.innerHeight);
+        setInnerWidth(window.innerWidth);
+      }, 100);
     };
     window.addEventListener('resize', listener);
 
