a4b8e72478c37fef9da105eed01540d4f29ced76
Colson <colsoncalifornication@gmail.com>
2023-10-19 20:06:15 +0545

    toggle markdown editor mode

--- src/components/text-editor.tsx
+++ src/components/text-editor.tsx
@@ -1,8 +1,30 @@
+import { useState, useEffect } from 'react';
 import MDEditor from '@uiw/react-md-editor';
 
 const TextEditor: React.FC = () => {
+  const [editing, setEditing] = useState(false);
+
+  useEffect(() => {
+    const listener = () => {
+      setEditing(false);
+    };  
+    document.addEventListener('click', listener, { capture: true });
+
+    return () => {
+      document.removeEventListener('click', listener, { capture: true });
+    }; 
+  }, []);
+
+  if (editing) {
+    return (
+      <div>
+        <MDEditor />
+      </div>
+    );
+  }
+
   return (
-    <div>
+    <div onClick={() => setEditing(true)} >
       <MDEditor.Markdown source={'# iDX'} />
     </div>
   );
