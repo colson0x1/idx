11d1688fbfea5d7ca0565922d92d676b95332d1c
Colson <colsoncalifornication@gmail.com>
2023-10-20 23:07:23 +0545

    style markdown layout

--- src/components/text-editor.tsx
+++ src/components/text-editor.tsx
@@ -35,8 +35,10 @@ const TextEditor: React.FC = () => {
   }
 
   return (
-    <div className="text-editor" onClick={() => setEditing(true)}>
-      <MDEditor.Markdown source={value} />
+    <div className="text-editor card" onClick={() => setEditing(true)}>
+      <div className="card-content">
+        <MDEditor.Markdown source={value} />
+      </div>
     </div>
   );
 };
