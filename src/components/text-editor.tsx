4c5e811210a974866797da15c08c6df6a285d006
Colson <colsoncalifornication@gmail.com>
2023-10-20 22:55:14 +0545

    add state to the editor

--- src/components/text-editor.tsx
+++ src/components/text-editor.tsx
@@ -5,6 +5,7 @@ import MDEditor from '@uiw/react-md-editor';
 const TextEditor: React.FC = () => {
   const ref = useRef<HTMLDivElement | null>(null);
   const [editing, setEditing] = useState(false);
+  const [value, setValue] = useState('# iDX');
 
   useEffect(() => {
     const listener = (event: MouseEvent) => {
@@ -28,14 +29,14 @@ const TextEditor: React.FC = () => {
   if (editing) {
     return (
       <div className="text-editor" ref={ref}>
-        <MDEditor />
+        <MDEditor value={value} onChange={(v) => setValue(v || '')} />
       </div>
     );
   }
 
   return (
     <div className="text-editor" onClick={() => setEditing(true)}>
-      <MDEditor.Markdown source={'# iDX'} />
+      <MDEditor.Markdown source={value} />
     </div>
   );
 };
