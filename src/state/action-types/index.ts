ea96644de93db57fd965b688bc2cd6ec8176153c
Colson <colsoncalifornication@gmail.com>
2023-10-21 13:57:14 +0545

    add action interface definitions

--- src/state/action-types/index.ts
+++ src/state/action-types/index.ts
@@ -1,6 +1,6 @@
-export enum ActionTypes = {
+export enum ActionType {
   MOVE_CELL = 'move_cell',
   DELETE_CELL = 'delete_cell',
   INSERT_CELL_BEFORE = 'insert_cell_before',
-  UPDATE_CELL = 'update_cell'
+  UPDATE_CELL = 'update_cell',
 }
