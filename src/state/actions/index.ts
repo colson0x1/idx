6737a5c6688776fe8cfc87045e9757c11a30a9be
Colson <colsoncalifornication@gmail.com>
2023-10-21 14:08:31 +0545

    add interface for cell

--- src/state/actions/index.ts
+++ src/state/actions/index.ts
@@ -1,4 +1,5 @@
 import { ActionType } from '../action-types';
+import { CellTypes } from '../cell';
 
 interface MoveCellAction {
   type: ActionType.MOVE_CELL;
@@ -17,7 +18,7 @@ interface InsertCellBeforeAction {
   type: ActionType.INSERT_CELL_BEFORE;
   payload: {
     id: string;
-    type: 'code' | 'text';
+    type: CellTypes;
   };
 }
 
