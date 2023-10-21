2e922fb8771e95cac12ce5e7bedd1cb5283d2f4b
Colson <colsoncalifornication@gmail.com>
2023-10-21 16:28:59 +0545

    implement action creators

--- src/state/action-creators/index.ts
+++ src/state/action-creators/index.ts
@@ -6,11 +6,47 @@ import {
   MoveCellAction,
   InsertCellBeforeAction,
 } from '../actions';
+import { CellTypes } from '../cell';
 
-export const updateCell = (): UpdateCellAction => {};
+export const updateCell = (id: string, content: string): UpdateCellAction => {
+  return {
+    type: ActionType.UPDATE_CELL,
+    payload: {
+      id,
+      content,
+    },
+  };
+};
 
-export const deleteCell = (): DeleteCellAction => {};
+export const deleteCell = (id: string): DeleteCellAction => {
+  return {
+    type: ActionType.DELETE_CELL,
+    payload: id,
+  };
+};
 
-export const moveCell = (): MoveCellAction => {};
+export const moveCell = (
+  id: string,
+  direction: 'up' | 'down'
+): MoveCellAction => {
+  return {
+    type: ActionType.MOVE_CELL,
+    payload: {
+      id,
+      direction,
+    },
+  };
+};
 
-export const insertCellBefore = (): InsertCellBeforeAction => {};
+export const insertCellBefore = (
+  id: string,
+  cellType: CellTypes
+): InsertCellBeforeAction => {
+  return {
+    type: ActionType.INSERT_CELL_BEFORE,
+    payload: {
+      id,
+      type: cellType,
+    },
+  };
+};
