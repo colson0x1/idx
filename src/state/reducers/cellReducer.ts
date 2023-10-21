5c91927ca48c1c2d97e434ab1e317ee256b95466
Colson <colsoncalifornication@gmail.com>
2023-10-21 14:31:47 +0545

    implement cell reducer

---
+++ src/state/reducers/cellReducer.ts
@@ -0,0 +1,28 @@
+import { ActionType } from '../action-types';
+import { Action } from '../actions';
+import { Cell } from '../cell';
+
+interface CellState {
+  loading: boolean;
+  error: string | null;
+  order: string[];
+  data: {
+    [key: string]: Cell;
+  };
+}
+
+const initialState: CellState = {
+  loading: false,
+  error: null,
+  order: [],
+  data: {},
+};
+
+const reducer = (
+  state: CellState = initialState,
+  action: Action
+): CellState => {
+  return state;
+};
+
+export default reducer;
