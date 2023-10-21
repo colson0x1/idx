5f96bd0fa22cd89cec9f20a672b63d5529a4b08f
Colson <colsoncalifornication@gmail.com>
2023-10-21 15:12:31 +0545

    create & central export redux store

---
+++ src/state/index.ts
@@ -0,0 +1,3 @@
+// Central export point for everything related to REDUX side of iDX
+export * from './store';
+export * from './reducers';
--- src/state/reducers/cellsReducer.ts
+++
@@ -1,4 +0,0 @@
-import { ActionTypes } from '../action-types'
-import { Action } from '../actions'
-
-interface CellState
