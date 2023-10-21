92909f85f22a5dd5cb7e3c9c4e48ce0cc5c24949
Colson <colsoncalifornication@gmail.com>
2023-10-21 13:14:08 +0545

    architect state with redux + create action types

--- src/components/text-editor.tsx
+++ src/components/text-editor.tsx
@@ -1,11 +1,11 @@
-import './text-editor.css';
-import { useState, useEffect, useRef } from 'react';
-import MDEditor from '@uiw/react-md-editor';
+import './text-editor.css'
+import { useState, useEffect, useRef } from 'react'
+import MDEditor from '@uiw/react-md-editor'
 
 const TextEditor: React.FC = () => {
-  const ref = useRef<HTMLDivElement | null>(null);
-  const [editing, setEditing] = useState(false);
-  const [value, setValue] = useState('# iDX');
+  const ref = useRef<HTMLDivElement | null>(null)
+  const [editing, setEditing] = useState(false)
+  const [value, setValue] = useState('# iDX')
 
   useEffect(() => {
     const listener = (event: MouseEvent) => {
@@ -14,33 +14,33 @@ const TextEditor: React.FC = () => {
         event.target &&
         ref.current.contains(event.target as Node)
       ) {
-        return;
+        return
       }
 
-      setEditing(false);
-    };
-    document.addEventListener('click', listener, { capture: true });
+      setEditing(false)
+    }
+    document.addEventListener('click', listener, { capture: true })
 
     return () => {
-      document.removeEventListener('click', listener, { capture: true });
-    };
-  }, []);
+      document.removeEventListener('click', listener, { capture: true })
+    }
+  }, [])
 
   if (editing) {
     return (
-      <div className="text-editor" ref={ref}>
+      <div className='text-editor' ref={ref}>
         <MDEditor value={value} onChange={(v) => setValue(v || '')} />
       </div>
-    );
+    )
   }
 
   return (
-    <div className="text-editor card" onClick={() => setEditing(true)}>
-      <div className="card-content">
+    <div className='text-editor card' onClick={() => setEditing(true)}>
+      <div className='card-content'>
         <MDEditor.Markdown source={value} />
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default TextEditor;
+export default TextEditor
--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,16 @@
-import "bulmaswatch/superhero/bulmaswatch.min.css";
-import ReactDOM from "react-dom/client";
-import TextEditor from "./components/text-editor";
+import 'bulmaswatch/superhero/bulmaswatch.min.css'
+import ReactDOM from 'react-dom/client'
+import TextEditor from './components/text-editor'
 
 const App = () => {
   return (
     <div>
       <TextEditor />
     </div>
-  );
-};
+  )
+}
 
-const container = document.querySelector("#root") as HTMLElement;
-const root = ReactDOM.createRoot(container);
+const container = document.querySelector('#root') as HTMLElement
+const root = ReactDOM.createRoot(container)
 
-root.render(<App />);
+root.render(<App />)
