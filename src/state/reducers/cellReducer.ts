f7249bfbea343cb406b152c5fa8cd27388f8196d
Colson <colsoncalifornication@gmail.com>
2023-10-28 08:57:05 +0545

    return state object to make TS happy with immer on reducer

--- src/state/reducers/cellReducer.ts
+++ src/state/reducers/cellReducer.ts
@@ -26,7 +26,8 @@ const reducer = produce((state: CellState = initialState, action: Action) => {
 
       // Using Immer
       state.data[id].content = content;
-      return;
+
+      return state;
 
     /* @ Without Immer / Vanilla Redux  
         return {
@@ -43,20 +44,21 @@ const reducer = produce((state: CellState = initialState, action: Action) => {
     case ActionType.DELETE_CELL:
       delete state.data[action.payload];
       state.order = state.order.filter((id) => id !== action.payload);
-      return;
+
+      return state;
     case ActionType.MOVE_CELL:
       const { direction } = action.payload;
       const index = state.order.findIndex((id) => id === action.payload.id);
       const targetIndex = direction === 'up' ? index - 1 : index + 1;
 
       if (targetIndex < 0 || targetIndex > state.order.length - 1) {
-        return;
+        return state;
       }
 
       state.order[index] = state.order[targetIndex];
       state.order[targetIndex] = action.payload.id;
 
-      return;
+      return state;
     case ActionType.INSERT_CELL_BEFORE:
       const cell: Cell = {
         content: '',
