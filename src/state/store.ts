798a42221560f83ce5dc011f8ad90da07fa749cb
Colson <colsoncalifornication@gmail.com>
2023-10-28 09:39:22 +0545

    do manual testing on redux store

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,5 +1,27 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import reducers from './reducers';
+import { ActionType } from './action-types';
 
 export const store = createStore(reducers, {}, applyMiddleware(thunk));
+
+store.dispatch({
+  type: ActionType.INSERT_CELL_BEFORE,
+  payload: {
+    id: null, // There is no other cell already inside this reducer that we can insert a new cell before
+    type: 'code',
+  },
+});
+
+store.dispatch({
+  type: ActionType.INSERT_CELL_BEFORE,
+  payload: {
+    id: null,
+    type: 'text',
+  },
+});
+
+// Retrieve cells id
+const id = store.getState().cell.order[0];
+
+console.log(store.getState());
