96b263568a8eb8fa59e6ce277aa89f49c8db50bb
Colson <colsoncalifornication@gmail.com>
2023-10-29 10:02:28 +0545

    dispatch function manually without hook

--- src/components/cell-list-item.tsx
+++ src/components/cell-list-item.tsx
@@ -1,12 +1,18 @@
 import { Cell } from '../state';
 import CodeShell from './code-shell';
 import TextEditor from './text-editor';
+import { useDispatch } from 'react-redux';
+import { actionCreators } from '../store';
 
 interface CellListItemProps {
   cell: Cell;
 }
 
 const CellListItem: React.FC<CellListItemProps> = ({ cell }) => {
+  const dispatch = useDispatch();
+
+  dispatch(actionCreators.updateCell('001', 'coding is awesome!'));
+
   let child: JSX.Element;
   if (cell.type === 'code') {
     child = <CodeShell />;
