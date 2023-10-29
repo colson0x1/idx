051c5ecac5658c9e681d1bcb67f62bd04ed895c7
Colson <colsoncalifornication@gmail.com>
2023-10-29 09:10:18 +0545

    select an ordered list

--- src/components/cell-list.tsx
+++ src/components/cell-list.tsx
@@ -1,7 +1,9 @@
 import { useTypedSelector } from '../hooks/use-typed-selector';
 
 const CellList: React.FC = () => {
-  useTypedSelector((state) => state);
+  const cell = useTypedSelector(({ cell: { order, data } }) =>
+    order.map((id) => data[id])
+  );
 
   return <div>Cell List</div>;
 };
