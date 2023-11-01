cf39c759948cc93a87ef361e5bdbe56ad7f2057b
Colson <colsoncalifornication@gmail.com>
2023-11-01 20:25:18 +0545

    style positioning of action bars

--- src/components/action-bar.tsx
+++ src/components/action-bar.tsx
@@ -1,3 +1,4 @@
+import './action-bar.css';
 import { useActions } from '../hooks/use-actions';
 
 interface ActionBarProps {
@@ -8,7 +9,7 @@ const ActionBar: React.FC<ActionBarProps> = ({ id }) => {
   const { moveCell, deleteCell } = useActions();
 
   return (
-    <div>
+    <div className='action-bar'>
       <button
         className='button is-primary is-small'
         onClick={() => moveCell(id, 'up')}
--- src/components/cell-list-item.tsx
+++ src/components/cell-list-item.tsx
@@ -1,3 +1,4 @@
+import './cell-list-item.css';
 import { Cell } from '../state';
 import CodeShell from './code-shell';
 import TextEditor from './text-editor';
@@ -16,9 +17,9 @@ const CellListItem: React.FC<CellListItemProps> = ({ cell }) => {
   }
 
   return (
-    <div>
-      <ActionBar id={cell.id} />
+    <div className='cell-list-item'>
       {child}
+      <ActionBar id={cell.id} />
     </div>
   );
 };
