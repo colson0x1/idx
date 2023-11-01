353c70f33329a4c9f24dd86612ad849a9bc7fc8a
Colson <colsoncalifornication@gmail.com>
2023-11-01 20:40:49 +0545

    add the bar over code-editor and write css effect for the action-bar

--- src/components/cell-list-item.tsx
+++ src/components/cell-list-item.tsx
@@ -11,17 +11,24 @@ interface CellListItemProps {
 const CellListItem: React.FC<CellListItemProps> = ({ cell }) => {
   let child: JSX.Element;
   if (cell.type === 'code') {
-    child = <CodeShell cell={cell} />;
+    child = (
+      <>
+        <div className='action-bar-wrapper'>
+          <ActionBar id={cell.id} />
+        </div>
+        <CodeShell cell={cell} />
+      </>
+    );
   } else {
-    child = <TextEditor cell={cell} />;
+    child = (
+      <>
+        <TextEditor cell={cell} />
+        <ActionBar id={cell.id} />
+      </>
+    );
   }
 
-  return (
-    <div className='cell-list-item'>
-      {child}
-      <ActionBar id={cell.id} />
-    </div>
-  );
+  return <div className='cell-list-item'>{child}</div>;
 };
 
 export default CellListItem;
