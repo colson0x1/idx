c5a974d86505c296f622e49b0e4c67bf12a3bf03
Colson <colsoncalifornication@gmail.com>
2023-11-01 20:02:03 +0545

    add icons buttons

--- src/components/action-bar.tsx
+++ src/components/action-bar.tsx
@@ -9,9 +9,30 @@ const ActionBar: React.FC<ActionBarProps> = ({ id }) => {
 
   return (
     <div>
-      <button onClick={() => moveCell(id, 'up')}>Up</button>
-      <button onClick={() => moveCell(id, 'down')}>Down</button>
-      <button onClick={() => deleteCell(id)}>Delete</button>
+      <button
+        className='button is-primary is-small'
+        onClick={() => moveCell(id, 'up')}
+      >
+        <span className='icon'>
+          <i className='fas fa-arrow-up' />
+        </span>
+      </button>
+      <button
+        className='button is-primary is-small'
+        onClick={() => moveCell(id, 'down')}
+      >
+        <span className='icon'>
+          <i className='fas fa-arrow-down' />
+        </span>
+      </button>
+      <button
+        className='button is-primary is-small'
+        onClick={() => deleteCell(id)}
+      >
+        <span className='icon'>
+          <i className='fas fa-times' />
+        </span>
+      </button>
     </div>
   );
 };
