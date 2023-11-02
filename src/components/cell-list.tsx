e23713b26a559e1bae6dc2d0e235f5f0a576ed9c
Colson <colsoncalifornication@gmail.com>
2023-11-02 09:42:58 +0545

    force add cell visibility when iDX window is null

--- src/components/cell-list.tsx
+++ src/components/cell-list.tsx
@@ -18,7 +18,7 @@ const CellList: React.FC = () => {
   return (
     <div>
       {renderedCells}
-      <AddCell nextCellId={null} />
+      <AddCell forceVisible={cells.length === 0} nextCellId={null} />
     </div>
   );
 };
