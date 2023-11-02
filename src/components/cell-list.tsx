6e4a136bdbac54d7a05261ec690c9d61c9515218
Colson <colsoncalifornication@gmail.com>
2023-11-02 08:22:37 +0545

    resolve key warning with react fragment

--- src/components/cell-list.tsx
+++ src/components/cell-list.tsx
@@ -1,3 +1,4 @@
+import { Fragment } from 'react';
 import { useTypedSelector } from '../hooks/use-typed-selector';
 import CellListItem from './cell-list-item';
 import AddCell from './add-cell';
@@ -8,10 +9,10 @@ const CellList: React.FC = () => {
   );
 
   const renderedCells = cells.map((cell) => (
-    <>
+    <Fragment key={cell.id}>
       <AddCell nextCellId={cell.id} />
-      <CellListItem key={cell.id} cell={cell} />
-    </>
+      <CellListItem cell={cell} />
+    </Fragment>
   ));
 
   return (
