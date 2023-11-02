aa629d9839a39f419c6f5f2b9368f8525353c984
Colson <colsoncalifornication@gmail.com>
2023-11-02 08:39:17 +0545

    style add cell

--- src/components/add-cell.tsx
+++ src/components/add-cell.tsx
@@ -9,9 +9,10 @@ const AddCell: React.FC<AddCellProps> = ({ nextCellId }) => {
   const { insertCellBefore } = useActions();
 
   return (
-    <div>
+    <div className='add-cell'>
       <button onClick={() => insertCellBefore(nextCellId, 'code')}>Code</button>
       <button onClick={() => insertCellBefore(nextCellId, 'text')}>Text</button>
+      <div className='divider'></div>
     </div>
   );
 };
