b47f95321bc68e8087444c618dae5cf012096036
Colson <colsoncalifornication@gmail.com>
2023-11-02 07:44:42 +0545

    add new cells

---
+++ src/components/add-cell.tsx
@@ -0,0 +1,18 @@
+import './add-cell.css';
+import { useActions } from '../hooks/use-actions';
+
+interface AddCellProps {
+  nextCellId: string;
+}
+
+const AddCell: React.FC<AddCellProps> = () => {
+  const { insertCellBefore } = useActions();
+  return (
+    <div>
+      <button onClick={() => insertCellBefore(nextCellId, 'code')}>Code</button>
+      <button onClick={() => insertCellBefore(nextCellId, 'text')}>Text</button>
+    </div>
+  );
+};
+
+export default AddCell;
