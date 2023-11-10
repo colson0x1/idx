d8773c9d2ace256b47d48f29937b59a098e5416e
Colson <colsoncalifornication@gmail.com>
2023-11-10 14:21:52 +0545

    style progress bar

--- src/components/code-shell.tsx
+++ src/components/code-shell.tsx
@@ -1,3 +1,4 @@
+import './code-shell.css';
 import { useEffect } from 'react';
 import CodeEditor from './code-editor';
 import Preview from './preview';
@@ -49,7 +50,11 @@ const CodeShell: React.FC<CodeCellProps> = ({ cell }) => {
           />
         </Resizable>
         {!bundle || bundle.loading ? (
-          <div>Loading...</div>
+          <div className='progress-cover'>
+            <progress className='progress is-small is-primary' max='100'>
+              Loading
+            </progress>
+          </div>
         ) : (
           <Preview code={bundle.code} bundlingStatus={bundle.err} />
         )}
