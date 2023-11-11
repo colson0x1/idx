3bfac02173cc11bace07a664911cff37c0ec9118
Colson <colsoncalifornication@gmail.com>
2023-11-11 09:03:56 +0545

    resolve naming collisions

--- src/components/code-shell.tsx
+++ src/components/code-shell.tsx
@@ -21,15 +21,15 @@ const CodeShell: React.FC<CodeCellProps> = ({ cell }) => {
 
     const cumulativeCode = [
       `
-        import React from 'react';
-        import ReactDOM from 'react-dom';
+        import _React from 'react';
+        import _ReactDOM from 'react-dom';
         
         const show = (value) => {
           const root = document.querySelector('#root');
 
           if (typeof value === 'object') {
             if (value.$$typeof && value.props) {
-              ReactDOM.render(value, root);
+              _ReactDOM.render(value, root);
             } else {
               root.innerHTML = JSON.stringify(value);
             }
