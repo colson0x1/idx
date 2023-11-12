4102e32043646dd4ce2d7b85c7e96094a4b47f0f
Colson <colsoncalifornication@gmail.com>
2023-11-12 22:38:47 +0545

    communicate to the API from the CLI

--- packages/cli/dist/commands/serve.js
+++ packages/cli/dist/commands/serve.js
@@ -2,10 +2,11 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.serveCommand = void 0;
 const commander_1 = require("commander");
+const local_api_1 = require("local-api");
 exports.serveCommand = new commander_1.Command()
     .command('serve [filename]')
     .description('Open a file for editing')
     .option('-p, --port <number>', 'port to run server on', '3001')
     .action((filename = 'adventOfCode.js', options) => {
-    console.log(filename, options);
+    (0, local_api_1.serve)(parseInt(options.port), filename, '/');
 });
