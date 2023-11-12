3444aece4323f28197a4f9726658353be9961546
Colson <colsoncalifornication@gmail.com>
2023-11-12 22:12:05 +0545

    specify command line options

--- packages/cli/dist/commands/serve.js
+++ packages/cli/dist/commands/serve.js
@@ -3,8 +3,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.serveCommand = void 0;
 const commander_1 = require("commander");
 exports.serveCommand = new commander_1.Command()
-    .command('serve')
+    .command('serve [filename]')
     .description('Open a file for editing')
-    .action(() => {
-    console.log('Getting ready to serve a file');
+    .option('-p, --port <number>', 'port to run server on', '3001')
+    .action((filename = 'adventOfCode.js', options) => {
+    console.log(filename, options);
 });
--- packages/cli/src/commands/serve.ts
+++ packages/cli/src/commands/serve.ts
@@ -1,8 +1,9 @@
 import { Command } from 'commander';
 
 export const serveCommand = new Command()
-  .command('serve')
+  .command('serve [filename]')
   .description('Open a file for editing')
-  .action(() => {
-    console.log('Getting ready to serve a file');
+  .option('-p, --port <number>', 'port to run server on', '3001')
+  .action((filename = 'adventOfCode.js', options) => {
+    console.log(filename, options);
   });
