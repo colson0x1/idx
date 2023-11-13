04695d2d61a6308f1a1563a8f07bad44a4d61717
Colson <colsoncalifornication@gmail.com>
2023-11-13 14:53:48 +0545

    add dependencies + configure express server

--- packages/local-api/dist/index.js
+++ packages/local-api/dist/index.js
@@ -1,9 +1,14 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.serve = void 0;
+const express_1 = __importDefault(require("express"));
 const serve = (port, filename, dir) => {
-    console.log('serving traffic on port', port);
-    console.log('saving/fetching cells from', filename);
-    console.log('that file is in dir', dir);
+    const app = (0, express_1.default)();
+    app.listen(port, () => {
+        console.log('Listening on port', port);
+    });
 };
 exports.serve = serve;
--- packages/local-api/src/index.ts
+++ packages/local-api/src/index.ts
@@ -1,5 +1,9 @@
+import express from 'express';
+
 export const serve = (port: number, filename: string, dir: string) => {
-  console.log('serving traffic on port', port);
-  console.log('saving/fetching cells from', filename);
-  console.log('that file is in dir', dir);
+  const app = express();
+
+  app.listen(port, () => {
+    console.log('Listening on port', port);
+  });
 };
