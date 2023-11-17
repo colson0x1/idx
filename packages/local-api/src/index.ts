6703c8f0772af9141d956c4437e37d54d15c7fe3
Colson <colsoncalifornication@gmail.com>
2023-11-17 15:50:05 +0545

    configure proxy middleware

--- packages/local-api/src/index.ts
+++ packages/local-api/src/index.ts
@@ -1,7 +1,18 @@
 import express from 'express';
+import { createProxyMiddleware } from 'http-proxy-middleware';
 
 export const serve = (port: number, filename: string, dir: string) => {
   const app = express();
+
+  app.use(
+    createProxyMiddleware({
+      target: 'http://localhost:3000',
+      // Enable websockets support
+      // Note: CRA by default uses websockets to tell the browser wheneven some development file is changed
+      ws: true,
+      logLevel: 'silent',
+    }),
+  );
 
   return new Promise<void>((resolve, reject) => {
     app.listen(port, resolve).on('error', reject);
