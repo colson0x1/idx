f2a92160f97d63c3bc005c4f9c4c537934527ec0
Colson <colsoncalifornication@gmail.com>
2023-11-18 13:57:29 +0545

    write cells to a file

--- packages/local-api/dist/routes/cells.js
+++ packages/local-api/dist/routes/cells.js
@@ -14,8 +14,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.createCellsRouter = void 0;
 const express_1 = __importDefault(require("express"));
+const promises_1 = __importDefault(require("fs/promises"));
+const path_1 = __importDefault(require("path"));
 const createCellsRouter = (filename, dir) => {
     const router = express_1.default.Router();
+    const fullPath = path_1.default.join(dir, filename);
     router.get('/cells', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
         // Make sure the cell storage file exists
         // If it does not exist, add in a default list of cells
@@ -24,11 +27,12 @@ const createCellsRouter = (filename, dir) => {
         // Send list of cells back to browser
     }));
     router.post('/cells', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-        // Make sure the file exists
-        // It not, create it
         // Take the list of cells from the request obj
         // serialize them
+        const { cells } = req.body;
         // Write the cells into the file
+        yield promises_1.default.writeFile(fullPath, JSON.stringify(cells), 'utf-8');
+        res.send({ status: 'ok' });
     }));
     return router;
 };
--- packages/local-api/src/routes/cells.ts
+++ packages/local-api/src/routes/cells.ts
@@ -1,7 +1,17 @@
 import express from 'express';
+import fs from 'fs/promises';
+import path from 'path';
+
+interface Cell {
+  id: string;
+  content: string;
+  type: 'text' | 'code';
+}
 
 export const createCellsRouter = (filename: string, dir: string) => {
   const router = express.Router();
+
+  const fullPath = path.join(dir, filename);
 
   router.get('/cells', async (req, res) => {
     // Make sure the cell storage file exists
@@ -12,11 +22,14 @@ export const createCellsRouter = (filename: string, dir: string) => {
   });
 
   router.post('/cells', async (req, res) => {
-    // Make sure the file exists
-    // It not, create it
     // Take the list of cells from the request obj
     // serialize them
+    const { cells }: { cells: Cell[] } = req.body;
+
     // Write the cells into the file
+    await fs.writeFile(fullPath, JSON.stringify(cells), 'utf-8');
+
+    res.send({ status: 'ok' });
   });
 
   return router;
