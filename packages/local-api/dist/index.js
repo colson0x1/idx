39648878f1dda87ccbba65ab8bb336dcae45a69c
Colson <colsoncalifornication@gmail.com>
2023-11-18 11:45:21 +0545

    resolve path for production build

--- packages/local-api/dist/index.js
+++ packages/local-api/dist/index.js
@@ -5,9 +5,36 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.serve = void 0;
 const express_1 = __importDefault(require("express"));
+const path_1 = __importDefault(require("path"));
 const serve = (port, filename, dir) => {
     const app = (0, express_1.default)();
-    app.use(express_1.default.static('../../local-client/build'));
+    /*
+     * @ The code below is configured to work on production environment :)
+     * I've written, Two different ways of serving react assets -
+     * : One is intended when user has installed CLI on to their local machine
+     * : The other is intended for whenever we're running our application
+     * in this kind of development mode and we've got an actual CRA server running
+     *
+     * require.resolve() doesn't attempt to load up a file instead, it's
+     * going to apply node's path resolution algorithm to figure out the
+     * directory or essentially the file location of that index.html file.
+     *
+     * packagePath right here is gonna be the absolute path on your machine
+     * to get to that index.html file.
+     *
+     * path.dirname() gives everything up to that build folder.
+     * express.static() is specifically going to serve that build folder.
+     *
+     * As soon as the user installs all the stuff on their local machine,
+     * require.resolve() is going to find the local-client build directory
+     * directly inside of our node_modules folder instead rather than giving us
+     * the local-client folder that's inside of packages.
+     *
+     * We only see local-client in packages with this line packagePath,
+     * when we're running our code in this sort of development environment
+     */
+    const packagePath = require.resolve('local-client/build/index.html');
+    app.use(express_1.default.static(path_1.default.dirname(packagePath)));
     /*
     app.use(
       createProxyMiddleware({
