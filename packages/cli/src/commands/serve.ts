4563464cd37378df3bb164ff81aac190da8827a7
Colson <colsoncalifornication@gmail.com>
2023-11-23 17:39:15 +0545

    hotfix: idxstarter.js filepath on user machine

--- packages/cli/src/commands/serve.ts
+++ packages/cli/src/commands/serve.ts
@@ -12,19 +12,16 @@ const isProduction = process.env.NODE_ENV === 'production';
 export const serveCommand = new Command()
   .command('serve [filename]')
   .description(
-    'Open a file for editing. \n    Try `colsidx serve idxstarter.js` to get started with iDX',
+    'Open a file for editing. \n    Try `colsidx serve idxstarter.js`',
   )
   .option('-p, --port <number>', 'port to run server on', '4004')
-  .action(async (filename = 'idxstarter.js', options: { port: string }) => {
+  .action(async (filename = 'idxAPI.js', options: { port: string }) => {
     const isLocalApiError = (err: any): err is LocalApiError => {
       return typeof err.code === 'string';
     };
 
     try {
-      const globalColsidxDir = path.resolve(
-        require.resolve('@colsidx/local-api/package.json'),
-        '..',
-      );
+      const globalColsidxDir = path.resolve(__dirname, '..');
 
       const defaultFile = ['idxstarter.js'];
       const isDefaultFile = defaultFile.includes(filename);
